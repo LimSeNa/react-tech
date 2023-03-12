189827fa5577b73b12b8e1b1793efe573336c197
Sena Lim <tpsk8322@naver.com>
2023-03-12 18:34:34 +0900

    함수의 파라미터에 구조 분해 문법 사용

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,5 +1,4 @@
-const MyComponent = props => {
-    const { name, children } = props;
+const MyComponent = ({ name, children }) => {
     return (
         <div>
             안녕하세요. 저의 이름은 {name}입니다. <br />
