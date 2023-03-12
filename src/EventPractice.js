5f5f1225d7e2b3df8b754eca89f293f40d2403ef
Sena Lim <tpsk8322@naver.com>
2023-03-13 03:58:19 +0900

    useState에 객체 넣기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -33,14 +33,14 @@ const EventPractice = () => {
                 name="username"
                 placeholder="사용자명"
                 value={username}
-                onChange={onChangeUsername}
+                onChange={onChange}
             />
             <input
                 type="text"
                 name="message"
                 placeholder="아무거나 입력해 보세요."
                 value={message}
-                onChange={onChangeMessge}
+                onChange={onChange}
             />
             <button onKeyPress={onKeyPress}>확인</button>
         </div>
