9883cc669557bb90f05262b7b57f9bbcf0c9a289
Sena Lim <tpsk8322@naver.com>
2023-03-20 11:13:15 +0900

    filter 함수를 사용하여 특정 항목 제거

--- src/chap06/InterationSample.js
+++ src/chap06/InterationSample.js
@@ -9,10 +9,28 @@ const InterationSample = () => {
     ]);
     const [inputText, setInputText] = useState('');
     const [nextId, setNextId] = useState(5);
+    const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId + 1); // nextId에 1을 더함
+        setNames(nextNames);        // names 값을 업데이트
+        setInputText('');     // inputText를 비움
+    }
+    const onRemove = id => {
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+    }
 
-    const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
+    const nameList = names.map(name => <li key={name.id} onDoubleClick={() => onRemove(name.id)}>{name.text}</li>);
     return (
-        <ul>{nameList}</ul>
+        <>
+            <input value={inputText} onChange={onChange}/>
+            <button onClick={onClick}>추가</button>
+            <ul>{nameList}</ul>
+        </>
     );
 };
 
